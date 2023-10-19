268c863c305d97e34ef3e63cab02ea768b4db03f
samuelabadie <91666297+samuelabadie@users.noreply.github.com>
2023-10-19 17:43:44 +0200

    routes config

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  # Your API routes
+  namespace :api do
+    namespace :app do
+      ressources :posts
+      ressources :actualities
+    end
+  end
   # root "articles#index"
 end
