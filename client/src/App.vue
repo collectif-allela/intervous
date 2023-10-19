9b5aca158a775b93187b94a372ac62f9f83bb80f
Andre Despouys <andredespouys@gmail.com>
2023-10-19 17:01:10 +0200

    Add Tailwind.css into client

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
     <h2>test</h2>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="logo vue bg-blue-300 p-5 " alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
