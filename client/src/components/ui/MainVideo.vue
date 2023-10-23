18a66c2e2a2e6e13b91cc81b13374cce104a3ceb
samuelabadie <91666297+samuelabadie@users.noreply.github.com>
2023-10-23 15:37:22 +0200

    main img component

---
+++ client/src/components/ui/MainVideo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="relative flex h-[80vh] w-[80vw] items-end justify-start">
+    <h2 class="text-beige z-10 mb-10 ml-[20px] text-6xl uppercase italic leading-none">
+      <span class="bg-blue break-words px-3">L'actu</span><br />
+      <span class="bg-blue ml-[20px] px-3">du 02/08</span>
+    </h2>
+    <div class="absolute z-0 h-full w-full bg-red-400 object-cover">
+      <img src="https://cdn.discordapp.com/attachments/898466011391610890/1165994781156057149/tanya-pro-jECS_UkyFZ0-unsplash.jpg?ex=6548e042&is=65366b42&hm=ad56288f9b2c4675a8008330bef2142a0a7c7086b2f3f0a6a4f2ce88855f7315&" alt="" class="h-full w-full object-cover" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainVideo',
+  // Your component's properties and methods here
+}
+</script>
