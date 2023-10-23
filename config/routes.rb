9bb2efc250b407ba8ae3f0989e2c6b2a52a774cf
samuelabadie <91666297+samuelabadie@users.noreply.github.com>
2023-10-23 12:15:15 +0200

    add devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :comments
   resources :actualities
   resources :posts
@@ -8,6 +9,16 @@ Rails.application.routes.draw do
   # Your API routes
   namespace :api do
     namespace :app do
+      # User Registration
+      mount_devise_token_auth_for 'User', at: 'auth'
+      # User Login
+      post 'auth/sign_in', to: 'user_token#create'
+      # User Logout (Destroy Token)
+      delete 'auth/sign_out', to: 'user_token#destroy'
+      # Token Validation
+      get 'auth/validate_token', to: 'user_token#validate_token'
+
+      resources :comments
       resources :posts
       resources :actualities
     end
