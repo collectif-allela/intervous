9d04a4682609651392564bb5299c3366e6cfd58a
samuelabadie <91666297+samuelabadie@users.noreply.github.com>
2023-10-23 16:02:29 +0200

    main video into home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,9 @@
 <script setup>
+import MainVideo from '../components/ui/MainVideo.vue';
 </script>
 <template>
 <h1> This is the Home User view</h1>
+<div id="app">
+  <main-video></main-video>
+</div>
 </template>
