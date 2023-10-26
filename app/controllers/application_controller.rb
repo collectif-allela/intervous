b7616cd2f106920dc3306ad6abb0c8996f1caf76
samuelabadie <91666297+samuelabadie@users.noreply.github.com>
2023-10-26 09:40:03 +0200

    gallery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::API
-        before_action :configure_permitted_parameters, if: :devise_controller?
-        include DeviseTokenAuth::Concerns::SetUserByToken
 
-        protected
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
       
-        def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_145107) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_162858) do
   create_table "actualities", force: :cascade do |t|
     t.string "title"
     t.text "video_url"
